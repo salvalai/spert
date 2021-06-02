482e793140756b0543189428f2666b3cacfd1d84
salvalai <m.salvalai001@studenti.unibs.it>
2021-06-02 20:58:14 +0200

    debugging

--- spert/loss.py
+++ spert/loss.py
@@ -33,9 +33,11 @@ class SpERTLoss(Loss):
         if rel_count.item() != 0:
             rel_logits = rel_logits.view(-1, rel_logits.shape[-1])
             print("rel_logits: ", rel_logits.shape)
+            print(rel_logits[:-1])
             rel_types = rel_types.view(-1, rel_types.shape[-1])
             print("rel_types: ", rel_types.shape)
-            
+            print(rel_types[:-1])
+
             rel_loss = self._rel_criterion(rel_logits, rel_types)
             #rel_loss = rel_loss.sum(-1) / rel_loss.shape[-1]
             rel_loss = (rel_loss * rel_sample_masks).sum() / rel_count
