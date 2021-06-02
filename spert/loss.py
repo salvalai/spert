46604d4b9da112080bd0248021b89298d8ff8a43
salvalai <m.salvalai001@studenti.unibs.it>
2021-06-02 20:11:51 +0200

    loss debug: problem with shapes

--- spert/loss.py
+++ spert/loss.py
@@ -33,7 +33,8 @@ class SpERTLoss(Loss):
         if rel_count.item() != 0:
             rel_logits = rel_logits.view(-1, rel_logits.shape[-1])
             #rel_types = rel_types.view(-1, rel_types.shape[-1])
-            rel_types = rel_types.view(-1)
+            print(rel_types)
+            # rel_types = rel_types.view(-1)
             
             print("Rel_logits: ", rel_logits.shape, ", rel_types", rel_types.shape)
             rel_loss = self._rel_criterion(rel_logits, rel_types)
